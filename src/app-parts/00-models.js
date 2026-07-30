da78952347d963aedd55d1f69cc269aca16b4cd0
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 00:25:04 +0300

    Wire model progress state into app

--- src/app-parts/00-models.js
+++ src/app-parts/00-models.js
@@ -1,2 +1,10 @@
 import { DEFAULT_LOCAL_MODEL_ID, LOCAL_MODEL_PROFILES, localModelProfile } from './ai.js';
 import { LOCAL_MODEL_ACTION, resolveLocalModelAction } from './services/model-action.js';
+import {
+  MODEL_LOAD_STATUS,
+  completeModelLoad,
+  createModelLoadState,
+  failModelLoad,
+  startModelLoad,
+  updateModelLoadProgress,
+} from './services/model-progress.js';
