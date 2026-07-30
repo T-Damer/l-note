bd37e0bcc16be6b3fb5726b9f568962abd91e625
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 00:18:57 +0300

    Expose the shared source card to the application shell

--- src/app-parts/00-ui.js
+++ src/app-parts/00-ui.js
@@ -1,3 +1,3 @@
-import { Button, Card, bindRoutedDialog } from './ui/components.js';
+import { Button, Card, SourceCard, bindRoutedDialog } from './ui/components.js';
 import { Icon, iconNameForSearchResult } from './ui/icons.js';
 import { Text } from './ui/text.js';
