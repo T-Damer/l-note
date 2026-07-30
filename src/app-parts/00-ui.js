b745f87c8b0cd67ed77883aff8dd442534c436a0
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 00:58:02 +0300

    Load the one-time welcome note service

--- src/app-parts/00-ui.js
+++ src/app-parts/00-ui.js
@@ -1,3 +1,4 @@
+import { ensureWelcomeNote } from './services/default-notes.js';
 import { Button, Card, Field, SourceCard, Switch, bindRoutedDialog } from './ui/components.js';
 import { Icon, iconNameForSearchResult } from './ui/icons.js';
 import { Text } from './ui/text.js';
