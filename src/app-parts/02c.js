9b0cc34677f3f2d36d8404c7885b37bc7a168f0d
Daniil Pankov <dpancov64@gmail.com>
2026-07-30 17:18:30 +0300

    Remove obsolete dialog stack code

--- src/app-parts/02c.js
+++ src/app-parts/02c.js
@@ -1,13 +1,3 @@
-        openNoteDialog(note);
-      });
-      list.append(button);
-    }
-    dom.entityDialogBody.append(list);
-  }
-  if (dom.documentDialog.open) dom.documentDialog.close();
-  if (!dom.entityDialog.open) dom.entityDialog.showModal();
-}
-
 async function renderCatalog() {
   const installedById = new Map(state.packRecords.map((record) => [record.id, record]));
   const entries = [...state.catalog.packs];
