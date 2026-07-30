68ff8b56e01dd5ac30a250b64bbef8e8f1b65cfc
Daniil Pankov <dpancov64@gmail.com>
2026-07-30 16:12:41 +0300

    Invalidate the PWA shell after model-lab changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = 'l-note-shell-v2';
-const RUNTIME_CACHE = 'l-note-runtime-v2';
+const SHELL_CACHE = 'l-note-shell-v3';
+const RUNTIME_CACHE = 'l-note-runtime-v3';
 const SHELL = [
   './',
   './index.html',
