e85dd8f68a019a1a7668993e22c82f0d5fd2e941
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 15:15:03 +0300

    Wire package resource renderer

--- src/app-parts/00-resources.js
+++ src/app-parts/00-resources.js
@@ -1,5 +1,6 @@
 import { detectEntitiesInText } from './helpers/entity-terms.js';
 import { renderConceptResource } from './pages/concept-resource-view.js';
 import { renderDocumentResource } from './pages/document-resource-view.js';
+import { renderPackageResource } from './pages/package-resource-view.js';
 import { createRoutedResourceRenderer } from './pages/routed-resource-renderer.js';
 import { renderStatementResource } from './pages/statement-resource-view.js';
