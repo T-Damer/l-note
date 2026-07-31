4478da7ffadb2f2e94551f3e27bc4899d052c01d
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 17:58:53 +0300

    Wire modular note dependencies

---
+++ src/app-parts/00-notes.js
@@ -0,0 +1,3 @@
+import { createNoteResourceView } from './pages/note-resource-view.js';
+import { renderNotesList } from './pages/notes-list-view.js';
+import { createNoteRecord, normalizeImportedNotes } from './services/note-workflow.js';
