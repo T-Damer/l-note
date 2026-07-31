e67a7cf45e13b47bf8112ba9c8b18cdf412ad5f6
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 14:52:43 +0300

    Wire modular resource renderers

---
+++ src/app-parts/00-resources.js
@@ -0,0 +1,4 @@
+import { renderConceptResource } from './pages/concept-resource-view.js';
+import { renderDocumentResource } from './pages/document-resource-view.js';
+import { createRoutedResourceRenderer } from './pages/routed-resource-renderer.js';
+import { renderStatementResource } from './pages/statement-resource-view.js';
