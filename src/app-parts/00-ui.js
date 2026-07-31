17f164a5d7dd8518a3b54a12492388cdf4cbaf37
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 15:09:16 +0300

    Use shared safe DOM helper in shell

--- src/app-parts/00-ui.js
+++ src/app-parts/00-ui.js
@@ -1,3 +1,4 @@
 import { Button, Card, Field, SourceCard, bindRoutedDialog } from './ui/components.js';
+import { element as create } from './ui/dom.js';
 import { Icon, iconNameForSearchResult } from './ui/icons.js';
 import { Text } from './ui/text.js';
