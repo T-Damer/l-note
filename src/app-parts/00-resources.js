fd64d8dcb0fd0a360ce5647b88bfb6fccfce2a6a
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 14:56:14 +0300

    Wire entity matching helper

--- src/app-parts/00-resources.js
+++ src/app-parts/00-resources.js
@@ -1,3 +1,4 @@
+import { detectEntitiesInText } from './helpers/entity-terms.js';
 import { renderConceptResource } from './pages/concept-resource-view.js';
 import { renderDocumentResource } from './pages/document-resource-view.js';
 import { createRoutedResourceRenderer } from './pages/routed-resource-renderer.js';
