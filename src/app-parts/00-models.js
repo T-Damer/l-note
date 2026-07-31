d0f628fe3492458f8d91ac8ee80c72851dddfc7e
Daniil Pankov <dpancov64@gmail.com>
2026-07-31 10:59:19 +0300

    Expose two local answer modes to the web shell

--- src/app-parts/00-models.js
+++ src/app-parts/00-models.js
@@ -1,4 +1,9 @@
 import { DEFAULT_LOCAL_MODEL_ID, LOCAL_MODEL_PROFILES, localModelProfile } from './ai.js';
+import {
+  ANSWER_MODE_PROFILES,
+  DEFAULT_ANSWER_MODE_ID,
+  answerModeProfile,
+} from './services/answer-modes.js';
 import { LOCAL_MODEL_ACTION, resolveLocalModelAction } from './services/model-action.js';
 import {
   MODEL_LOAD_STATUS,
