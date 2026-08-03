822ff123f59e9fce22fd12f75121b84ec3ec54dd
Daniil Pankov <dpancov64@gmail.com>
2026-08-03 16:59:53 +0300

    Validate the prebuilt search artifact manifest

--- src/packs.js
+++ src/packs.js
@@ -1,4 +1,4 @@
-import { validateSearchArtifact } from './helpers/search-artifacts.js';
+import { validatePrebuiltSearchArtifacts } from './helpers/prebuilt-search-artifacts.js';
 import { validateStatementRelations } from './helpers/statement-conflicts.js';
 
 const PACK_SCHEMA_VERSION = 1;
@@ -112,7 +112,7 @@ export function validatePack(pack) {
     if (relation?.targetId && !entityIds.has(relation.targetId)) errors.push(`relation references unknown target ${relation.targetId}`);
   }
 
-  errors.push(...validateSearchArtifact(pack.searchArtifact));
+  errors.push(...validatePrebuiltSearchArtifacts(pack));
   errors.push(...validateStatementRelations(pack));
   return { valid: errors.length === 0, errors };
 }
